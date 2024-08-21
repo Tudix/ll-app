cf9bdf57e81cc0e782791ca72dc572c4921215ce
Bogdan <bogdan3096@gmail.com>
2024-08-22 00:57:29 +0300

    Meta tags and components

---
+++ src/Footer.js
@@ -0,0 +1,31 @@
+function Footer(){
+    return(
+        <footer>
+            Navigation
+            <ul>
+                <li><a href="/Home">Home</a></li>
+                <li><a href="/About">About</a></li>
+                <li><a href="/Menu">Menu</a></li>
+                <li><a href="/Reservations">Reservations</a></li>
+                <li><a href="/Order">Order Online</a></li>
+                <li><a href="/Login">Login</a></li>
+            </ul>
+            Contact
+            <ul>
+                <li><a href="/Adress">Adress</a></li>
+                <li><a href="/Phone">Phone Number</a></li>
+                <li><a href="/Email">Email</a></li>
+            </ul>
+            Social Media Links
+            <ul>
+                <li><a href="/Instagram">Instagram</a></li>
+                <li><a href="/Facebook">Facebook</a></li>
+                <li><a href="/TikTok">Tiktok</a></li>
+                <li><a href="/Youtube">Youtube</a></li>
+                <li><a href="/X">X</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+function Nav(){
+    return(
+        <nav>
+            <img src="ceva" alt="logo_LittleLemon"></img>
+            <ul>
+                <li><a href="/Home">Home</a></li>
+                <li><a href="/About">About</a></li>
+                <li><a href="/Menu">Menu</a></li>
+                <li><a href="/Reservations">Reservations</a></li>
+                <li><a href="/Order">Order Online</a></li>
+                <li><a href="/Login">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
