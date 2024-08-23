f1ce169c8662179b369db09561da0b176d5d4626
Bogdan <bogdan3096@gmail.com>
2024-08-24 02:11:55 +0300

    First CSS touch

--- src/Footer.js
+++ src/Footer.js
@@ -1,29 +1,37 @@
 function Footer(){
     return(
         <footer>
-            Navigation
-            <ul>
-                <li><a href="/Home">Home</a></li>
-                <li><a href="/About">About</a></li>
-                <li><a href="/Menu">Menu</a></li>
-                <li><a href="/Reservations">Reservations</a></li>
-                <li><a href="/Order">Order Online</a></li>
-                <li><a href="/Login">Login</a></li>
-            </ul>
-            Contact
-            <ul>
-                <li><a href="/Adress">Adress</a></li>
-                <li><a href="/Phone">Phone Number</a></li>
-                <li><a href="/Email">Email</a></li>
-            </ul>
-            Social Media Links
-            <ul>
-                <li><a href="/Instagram">Instagram</a></li>
-                <li><a href="/Facebook">Facebook</a></li>
-                <li><a href="/TikTok">Tiktok</a></li>
-                <li><a href="/Youtube">Youtube</a></li>
-                <li><a href="/X">X</a></li>
-            </ul>
+            <img src={require("./icons_assets/logo.png")} alt="logo_LittleLemon" height={250}/>
+            <div>
+                <h5>Navigation</h5>
+                <ul class="nav_items_column">
+                    <li><a href="/Home">Home</a></li>
+                    <li><a href="/About">About</a></li>
+                    <li><a href="/Menu">Menu</a></li>
+                    <li><a href="/Reservations">Reservations</a></li>
+                    <li><a href="/Order">Order Online</a></li>
+                    <li><a href="/Login">Login</a></li>
+                </ul>
+            </div>
+            <div>
+            <h5>Contact</h5>
+                <ul class="nav_items_column">
+                    <li><a href="/Adress">Adress</a></li>
+                    <li><a href="/Phone">Phone Number</a></li>
+                    <li><a href="/Email">Email</a></li>
+                </ul>
+            </div>
+            <div>
+            <h5>Social Media Links</h5>
+                <ul class="nav_items_column">
+                    <li><a href="/Instagram">Instagram</a></li>
+                    <li><a href="/Facebook">Facebook</a></li>
+                    <li><a href="/TikTok">Tiktok</a></li>
+                    <li><a href="/Youtube">Youtube</a></li>
+                    <li><a href="/X">X</a></li>
+                </ul>
+            </div>
+            
         </footer>
     )
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,13 @@
 function Nav(){
     return(
         <nav>
-            <img src="ceva" alt="logo_LittleLemon"></img>
-            <ul>
+          <img src={require("./icons_assets/logo_header.png")} alt="logo_LittleLemon" height={50}/>
+            <ul class="nav_items_inline">
                 <li><a href="/Home">Home</a></li>
                 <li><a href="/About">About</a></li>
                 <li><a href="/Menu">Menu</a></li>
                 <li><a href="/Reservations">Reservations</a></li>
-                <li><a href="/Order">Order Online</a></li>
+                <li><a href="/Order">Order online</a></li>
                 <li><a href="/Login">Login</a></li>
             </ul>
         </nav>
